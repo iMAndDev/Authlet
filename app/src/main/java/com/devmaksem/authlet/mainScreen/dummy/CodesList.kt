fa27820d41273c997d5c97d417a91b3bd6b1d60e
Kirill <kirillvoinov408@gmail.com>
2021-01-26 21:35:30 +0200

    Fixes.

--- app/src/main/java/com/devmaksem/authlet/mainScreen/dummy/CodesList.kt
+++ app/src/main/java/com/devmaksem/authlet/mainScreen/dummy/CodesList.kt
@@ -1,12 +1,11 @@
 package com.devmaksem.authlet.mainScreen.dummy
 
 import com.devmaksem.authlet.mainScreen.item.ListItem
-import java.util.*
 
 /**
  * Dummy hash codes
  */
-var listCodes: ArrayList<ListItem> = arrayListOf(
-    ListItem("GitHub", "123456"),
-    ListItem("HRTC Education Platform", "694200" )
+var listCodes = arrayListOf<ListItem>(/*
+    ListItem("GitHub", "12"),
+    ListItem("HRTC Education Platform", "1" )*/
 )
